1913811c01ceaab5f54a84f903e2e56140469574
Eva <Eva@Nicolass-MacBook-Pro.local>
2020-02-06 22:05:00 -0500

    fixed the delete array

--- src/BankProject/UserIDManager.java
+++ src/BankProject/UserIDManager.java
@@ -21,8 +21,8 @@ public class UserIDManager {
         String[] newArray = new String[n + 1];
         for (int i = 0; i < n; i++) {
             newArray[i] = userNames[i];
-            newArray[n] = name;
         }
+        newArray[n] = name;
         return newArray;
     }
 
@@ -68,22 +68,33 @@ public class UserIDManager {
         return valid;
     }
 
-    public void deleteAccount(String userName) {
+    public boolean deleteAccount(String userName) {
       //  String[] array = new String[userNames.length];
+        boolean deleted = false;
+
         int newSize = 0;
+
+        // find the size of the new array with username removed
         for (int i = 0; i < userNames.length; i++) {
-            if (userNames[i].equals(userName)) {
-                newSize++;
+            if(userNames[i].equals(userName)) {
+                // DO NOTHING
             }
+            else
+                newSize++;
         }
+
+        // copy all old element in new array, except for element same as username
         String[] tempArray = new String[newSize];
         int index = 0;
         for (int i = 0; i < userNames.length; i++) {
             if (userNames[index].equals(userName)) {
+                deleted = true;
+            }
+            else{
                 tempArray[index] = userNames[i];
                 index++;
             }
         }
-
+        return deleted;
     }
 }
